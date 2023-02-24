fa7c434c6b23994b319ba8f6740546a2ec0f12a4
Anessa Whitlow <anessa_whitlow68@u.asumh.edu>
2023-02-24 07:18:16 -0600

    Week 6 Implement a Custom Control

--- FoodTrackerProject/ViewController.swift
+++ FoodTrackerProject/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var RatingControl: RatingControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -64,10 +65,6 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
         //Notify the ViewController when the user picks an image
         imagePickerController.delegate = self
         present(imagePickerController, animated: true, completion: nil)
-    }
-    
-    @IBAction func setDefaultLabelText(_ sender: UIButton) {
-        mealNameLabel.text = "Default Text"
     }
 }
 
