162b33edc2d98c264d8c4c1a69b45bed467af0a7
Anessa Whitlow <anessa_whitlow68@u.asumh.edu>
2023-03-07 11:40:43 -0600

    Week 8 Implement Navigation

--- FoodRaterApp/FoodTrackerProject/MealTableViewCell.swift
+++ FoodRaterApp/FoodTrackerProject/MealTableViewCell.swift
@@ -1,14 +1,13 @@
 //
 //  MealTableViewCell.swift
 //  FoodTrackerProject
-//
+
 
 import UIKit
 
 class MealTableViewCell: UITableViewCell {
     
     //MARK:Properties
-
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var ratingControl: RatingControl!
